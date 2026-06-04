6022f664efb33b8523dca7da9de319bbf0e33d4d
Bruno Amorim <brunojfgbilusbgbvdsg@gmail.com>
2026-06-04 18:01:29 -0300

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+const API_URL = 'https://hypersaveapi-production.up.railway.app';
+
 createApp(App).mount('#app')
